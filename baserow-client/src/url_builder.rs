50c63a0daf368e7cf9c1e189228e7e2d7b761319
Sönke Liebau <soenke.liebau@stackable.tech>
2025-06-13 17:52:52 +0200

    First steps toward something even resembling error handling.

--- baserow-client/src/url_builder.rs
+++ baserow-client/src/url_builder.rs
@@ -1,5 +1,15 @@
 use reqwest::Url;
+use snafu::{ResultExt, Snafu};
 use std::str::FromStr;
+
+#[derive(Snafu, Debug)]
+pub enum Error {
+    #[snafu(display("Unable to build url for {action}:  {source}"))]
+    BuildUrl {
+        source: url::ParseError,
+        action: String,
+    },
+}
 
 pub struct UrlBuilder {
     base_url: Url,
@@ -13,53 +23,66 @@ impl UrlBuilder {
     const LIST_TABLE_FIELDS_URL: &'static str = "/api/database/fields/table/";
     const RECORD_URL: &'static str = "/api/database/rows/table/";
 
-    pub fn new(base_url: Option<&str>) -> Self {
+    pub fn new(base_url: Option<&str>) -> Result<Self, Error> {
         match base_url {
-            None => Self::default(),
-            Some(url) => Self{ base_url: Url::from_str(url).unwrap() }
+            None => Ok(Self::default()),
+            Some(url) => Ok(Self {
+                base_url: Url::from_str(url).context(BuildUrlSnafu { action: "base url" })?,
+            }),
         }
-    }
-
-    pub fn get_list_tables_url(&self) -> Url {
-        self.base_url.join(Self::LIST_TABLES_URL).unwrap()
     }
 
     pub fn get_record_url(&self) -> Url {
         self.base_url.join(Self::RECORD_URL).unwrap()
     }
 
-    pub fn get_list_records_url(&self, table_id: usize) -> Url {
-        self.get_record_url().join(&table_id.to_string()).unwrap()
-    }
-
-    pub fn get_create_record_url(&self, table_id: usize) -> Url {
-        self.get_record_url().join(&table_id.to_string()).unwrap()
-    }
-
-    pub fn get_list_table_fields_url(&self) -> Url {
-        self.base_url.join(Self::LIST_TABLE_FIELDS_URL).unwrap()
-    }
-
-    pub fn get_find_record_url(&self, table_id: usize, field_id: &str, id: &str) -> Url {
-        self.get_record_url()
+    pub fn get_list_records_url(&self, table_id: usize) -> Result<Url, Error> {
+        Ok(self
+            .get_record_url()
             .join(&table_id.to_string())
-            .unwrap()
+            .context(BuildUrlSnafu {
+                action: "listing records",
+            })?)
+    }
+
+    pub fn get_create_record_url(&self, table_id: usize) -> Result<Url, Error> {
+        Ok(self
+            .get_record_url()
+            .join(&table_id.to_string())
+            .context(BuildUrlSnafu {
+                action: "creating record",
+            })?)
+    }
+
+    pub fn get_find_record_url(
+        &self,
+        table_id: usize,
+        field_id: &str,
+        id: &str,
+    ) -> Result<Url, Error> {
+        Ok(self
+            .get_create_record_url(table_id)?
             .join(&format!("?filter__{}__equal={}", field_id, id))
-            .unwrap()
+            .context(BuildUrlSnafu {
+                action: "finding record by id",
+            })?)
     }
 
-    pub fn get_update_record_url(&self, table_id: usize, record_id: usize) -> Url {
-        self.get_record_url()
-            .join(&table_id.to_string())
-            .unwrap()
+    pub fn get_update_record_url(&self, table_id: usize, record_id: usize) -> Result<Url, Error> {
+        Ok(self
+            .get_create_record_url(table_id)?
             .join(&record_id.to_string())
-            .unwrap()
+            .context(BuildUrlSnafu {
+                action: "updating record by id",
+            })?)
     }
 }
 
 impl Default for UrlBuilder {
     fn default() -> Self {
         Self {
+            // This unwrap is okay, if we ever hit that it is an error in the code, 
+            // as the parsed url is hard-coded
             base_url: Url::from_str(Self::CLOUD_URL).unwrap(),
         }
     }
